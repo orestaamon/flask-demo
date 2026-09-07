9d58631c7c4b0f1058ad447fb1dda3d69212db02
orestaamon <orestaamon@gmail.com>
2026-09-07 17:09:37 +0200

    changess done

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "<h1>Hei fra Flask!🎉</h1><p>Servert fra mitt virtuelle miljø 🎉</p>"
+    return "<h1>Hei fra Flask!🎉</h1><p>Dette er oppdatert versjon!! 🎉</p>"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
