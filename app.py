f15538f4fc1f216137d3a93ff032088c8b091f95
orestaamon <orestaamon@gmail.com>
2026-09-07 18:43:03 +0200

    Completed Flask + Docker exercises, updated config files

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
+
+load_dotenv()
 app = Flask(__name__)
+
 @app.route("/")
 def hello():
-    return "<h1>Hei fra Flask!🎉</h1><p>Dette er oppdatert versjon!! 🎉</p>"
+    hemmelighet = os.getenv("SECRET_MESSAGE", "ingen hemmelighet satt")
+    return f"<h1>Hei fra Flask!</h1><p>{hemmelighet}</p>"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
