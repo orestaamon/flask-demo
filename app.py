9c62836eb4a9e644925a55a0cd71630a246c98c5
orestaamon <orestaamon@gmail.com>
2026-09-07 15:30:48 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "<h1>Hei fra Flask!</h1><p>Servert fra mitt virtuelle miljø 🎉</p>"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
